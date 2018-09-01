ec9c4fccc7cf3e80bea1bb5d59f0475f870cdae8
darogha <41114164+darogha@users.noreply.github.com>
2018-09-01 23:39:32 +0500

    Add files via upload

---
+++ a.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>chk</title>
+</head>
+<body>
+
+<?php
+
+	echo "asdfasdfa";
+echo '<script type="text/javascript"> drawChart(); </script>';
+
+?>
+</body>
+</html>
